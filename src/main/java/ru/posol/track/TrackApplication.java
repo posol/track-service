16f8925e02eaad49bdb95bea8053b0fd8c362b70
POSOL\posol <posol2008@yandex.ru>
2018-07-30 23:05:38 +0500

    create project

---
+++ src/main/java/ru/posol/track/TrackApplication.java
@@ -0,0 +1,12 @@
+package ru.posol.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackApplication.class, args);
+	}
+}
